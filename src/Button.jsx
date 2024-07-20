5beab81dda325563bad3e7b4094403f7f1a6da6b
mfarrukh14 <87035938+mfarrukh14@users.noreply.github.com>
2024-07-20 21:10:40 +0500

    Testing the target properties of onClick event in button component

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,21 +1,10 @@
 
 export default function Button()
 {
-    let count = 0;
-    const handleClick = (name) => {
-        if (count < 3)
-        {
-            count++;
-            console.log(`You clicked me ${count} time\s`);
-        }
-
-        else{
-            console.log(`${name}, stop clicking me`);
-        }
-    }
+    const handleClick = (e) => e.target.textContent = "OUCH";
 
     return(
-        <button onClick={() => handleClick("Farrukh")}>Click Me</button>
+        <button onDoubleClick={(e) => handleClick(e)}>Click Me</button>
     );
 
 }
